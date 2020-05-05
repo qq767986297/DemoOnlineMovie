88405ece41203b6c1ed27eafd59daef480a26704
qq767986297 <767986297@qq.com>
2020-05-05 11:17:54 +0800

    5.5提交

--- app/src/main/java/com/bw/movie/fragment/FragmentDetailComment.java
+++ app/src/main/java/com/bw/movie/fragment/FragmentDetailComment.java
@@ -32,6 +32,7 @@ import butterknife.Unbinder;
  * Time: 2020/4/24
  * Author: 王冠华
  * Description:
+ * 1
  */
 public class FragmentDetailComment extends BaseFragment implements IDetailContract.IView {
     @BindView(R.id.rv_fragment_detail_comment)
